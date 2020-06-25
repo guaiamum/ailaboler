dba21259bbc4f376b769dc2430f1ab468dbf57cb
Cainã Brazil <CainaBrazil@gmail.com>
2020-06-25 11:11:05 +0100

    fixed preact build, removed preact type declarations

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,35 @@ import Head from 'next/head'
 import '../styles/main.sass'
 
 const Home = () => (
-  <div class="container">
+  <div className="container">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
-      <h1 class="title">
+      <h1 className="title">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
-      <p class="description">
+      <p className="description">
         Get started by editing <code>pages/index.js</code>
       </p>
 
-      <div class="grid">
-        <a href="https://nextjs.org/docs" class="card">
+      <div className="grid">
+        <a href="https://nextjs.org/docs" className="card">
           <h3>Documentation &rarr;</h3>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
-        <a href="https://nextjs.org/learn" class="card">
+        <a href="https://nextjs.org/learn" className="card">
           <h3>Learn &rarr;</h3>
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
         <a
           href="https://github.com/zeit/next.js/tree/master/examples"
-          class="card"
+          className="card"
         >
           <h3>Examples &rarr;</h3>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -38,7 +38,7 @@ const Home = () => (
 
         <a
           href="https://vercel.com/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          class="card"
+          className="card"
         >
           <h3>Deploy &rarr;</h3>
           <p>
