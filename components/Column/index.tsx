d8d9443a2940e0bcdbc36b89b534ce86d1e22ba8
Cainã Brazil <CainaBrazil@gmail.com>
2020-08-09 08:37:20 +0100

    style changes

--- components/Column/index.tsx
+++ components/Column/index.tsx
@@ -2,10 +2,10 @@ import './styles.sass'
 
 const Column = ({ title: columnTitle, items }: Column) => (
     <section className="column">
-        <h2>{columnTitle}</h2>
+        <h2 className="title">{columnTitle}</h2>
         {
             items.map(({ id, title, text }) => (
-                <article key={id}>
+                <article key={id} className="item">
                     <h3>{title}</h3>
                     <p>
                         {text}
